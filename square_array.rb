82b312eb7ef3c3618c7c9e9c42c84c74341425c1
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-24 21:47:14 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,6 +3,6 @@ numbers= ["1", "2", "3"]
 def square_array(numbers)
   square_numbers = []
   numbers.each do |number|
-    return square_numbers << number*2
+    square_numbers << number**2
   end
 end 
