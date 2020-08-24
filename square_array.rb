6b71519d3890d9734dd9f96cf6ed950c7827d8e0
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-24 21:56:06 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -7,3 +7,14 @@ def square_array(numbers)
   end
   return square_numbers
 end 
+
+
+new_numbers= ["9, 10","16","25"]
+
+def square_array (new_numbers)
+  square_numbers= []
+  new_numbers.each do |new_number|
+    square_numbers << new_number**2
+  end
+  return square_numbers
+end
